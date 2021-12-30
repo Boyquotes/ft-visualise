a15a8ca889d74bc483c78784768c101c73c45d5e
Jon Herbst <herbtaculargames@gmail.com>
2021-12-30 13:51:22 +0000

    Configuring second config menu styling to match that of the main menu

--- ft-visualise/Root.gd
+++ ft-visualise/Root.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var errors = []
-var labels = {}
+var labels_menu = {}
+var labels_config = {}
 
 var menu = null
 var config = null
@@ -36,28 +37,28 @@ func formatMenuElements(container : MarginContainer) -> void:
 						'LOAD PULSE 1':
 							element.connect('pressed', self, '_on_Pulse1_pressed')
 							
-							if not 'pulse1' in labels.keys():
-								labels['pulse1'] = element.get_parent().get_node('Pulse1Label')
+							if not 'pulse1' in labels_menu.keys():
+								labels_menu['pulse1'] = element.get_parent().get_node('Pulse1Label')
 						'LOAD PULSE 2':
 							element.connect('pressed', self, '_on_Pulse2_pressed')
 							
-							if not 'pulse2' in labels.keys():
-								labels['pulse2'] = element.get_parent().get_node('Pulse2Label')
+							if not 'pulse2' in labels_menu.keys():
+								labels_menu['pulse2'] = element.get_parent().get_node('Pulse2Label')
 						'LOAD TRIANGLE':
 							element.connect('pressed', self, '_on_Triangle_pressed')
 							
-							if not 'triangle' in labels.keys():
-								labels['triangle'] = element.get_parent().get_node('TriangleLabel')
+							if not 'triangle' in labels_menu.keys():
+								labels_menu['triangle'] = element.get_parent().get_node('TriangleLabel')
 						'LOAD NOISE':
 							element.connect('pressed', self, '_on_Noise_pressed')
 							
-							if not 'noise' in labels.keys():
-								labels['noise'] = element.get_parent().get_node('NoiseLabel')
+							if not 'noise' in labels_menu.keys():
+								labels_menu['noise'] = element.get_parent().get_node('NoiseLabel')
 						'LOAD DPCM':
 							element.connect('pressed', self, '_on_DPCM_pressed')
 							
-							if not 'dpcm' in labels.keys():
-								labels['dpcm'] = element.get_parent().get_node('DPCMLabel')
+							if not 'dpcm' in labels_menu.keys():
+								labels_menu['dpcm'] = element.get_parent().get_node('DPCMLabel')
 				elif element is Label:
 					element.set('custom_fonts/font', GlobalFont.getLabelFont())
 					
@@ -67,6 +68,42 @@ func formatMenuElements(container : MarginContainer) -> void:
 	if null != footer_button and footer_button is Button:
 		footer_button.set('custom_fonts/font', GlobalFont.getButtonFont())
 		footer_button.connect('pressed', self, '_on_ConfigButton_pressed')
+		
+func formatConfigElements(container : MarginContainer) -> void:
+	var header = container.find_node('ConfigTitleContainer')
+	var header_label = header.get_child(0)
+	
+	if null != header_label and header_label is Label:
+		header_label.set('custom_fonts/font', GlobalFont.getTitleFont())
+		
+	var button_container = container.find_node('ConfigLoadListContainer')
+	
+	if null != button_container:
+		for row in button_container.get_children():
+			for element in row.get_children():
+				if element is Button:
+					element.set('custom_fonts/font', GlobalFont.getButtonFont())
+					
+					match element.text.to_upper():
+						'LOAD JSON':
+							element.connect('pressed', self, '_on_JSON_pressed')
+							
+							if not 'json' in labels_config.keys():
+								labels_config['json'] = element.get_parent().get_node('JSONLabel')
+						'LOAD XML':
+							element.connect('pressed', self, '_on_XML_pressed')
+							
+							if not 'xml' in labels_config.keys():
+								labels_config['xml'] = element.get_parent().get_node('XMLLabel')
+				elif element is Label:
+					element.set('custom_fonts/font', GlobalFont.getLabelFont())
+					
+	var footer_container = container.find_node('LaunchContainer')
+	var footer_button = footer_container.get_child(0)
+	
+	if null != footer_button and footer_button is Button:
+		footer_button.set('custom_fonts/font', GlobalFont.getButtonFont())
+		footer_button.connect('pressed', self, '_on_LaunchButton_pressed')
 	
 func setupDialog(ident : String) -> void:
 	var dialog = create_instance('res://ft-visualise/FileHandler/Finder.tscn')
@@ -80,10 +117,10 @@ func setupDialog(ident : String) -> void:
 	dialog.connect('path_selected', self, '_on_FileFinder_path_selected')
 	
 func verifyPaths() -> bool:
-	if 5 != len(labels):
+	if 5 != len(labels_menu):
 		self.errors.append('Full path data not provided for parsing.')
 		
-	for path in labels.values():
+	for path in labels_menu.values():
 		if not path:
 			errors.append('Non-existent label object provided.')
 		elif not path.text:
@@ -131,6 +168,7 @@ func _on_ConfigButton_pressed() -> void:
 		menu.propagate_call('queue_free', [])
 		
 		self.add_child(config)
+		self.formatConfigElements(config)
 	else:
 		var notify_error = AcceptDialog.new()
 		notify_error.dialog_text = "Invalid file path(s) provided.\n\n" + self.getErrorStrings()
@@ -139,5 +177,5 @@ func _on_ConfigButton_pressed() -> void:
 		notify_error.popup()
 
 func _on_FileFinder_path_selected(path : String, ident : String):
-	if labels[ident]:
-		labels[ident].text = path
+	if labels_menu[ident]:
+		labels_menu[ident].text = path
