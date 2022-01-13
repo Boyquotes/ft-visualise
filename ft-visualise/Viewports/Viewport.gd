55770d57b04ef8cbc2b9e70eb6246d081b28b5c9
Jon Herbst <herbtaculargames@gmail.com>
2022-01-13 13:22:54 +0000

    Adding new viewport node to house visualiser instances for each channel and overarching song information

---
+++ ft-visualise/Viewports/Viewport.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Text storage for basic human-readable song information
+var song_author = ''
+var song_copy = ''
+var song_expansion = ''
+var song_title = ''
+
+# Song length and speed modifiers
+var song_bpm = 0
+var song_frames = 0
+var song_speed = 0
+
+# Frame IDs for each channel
+var song_patterns = []
